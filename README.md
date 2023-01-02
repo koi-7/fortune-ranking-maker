7f723c633b97da8063ff27a793103741ae4088e9
Koi <kkoiwa.1720+git@gmail.com>
2023-01-02 10:57:23 +0900

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/61448492/210188891-a191a711-1d6b-4ac3-acd2-0c0f697931e4.png">
+</p>
+
 # Fortune ranking maker
+
+運勢ランキングを表示するだけのツール
+
+# 使い方
+
+``` bash
+$ ./main.py
+```
+
+# 例
+
+![](https://user-images.githubusercontent.com/61448492/210190313-0e35bba0-b5f4-4d72-bd89-98d305a47936.png)
--- fortune-ranking-maker/main.py
+++ fortune-ranking-maker/main.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+
+import random
+
+
+ZODIAC_SIGN = ['牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座',
+               '天秤座', '蠍座', '射手座', '山羊座', '水瓶座', '魚座']
+JAPANESE_ZODIAC = ['子年', '丑年', '寅年', '卯年', '辰年', '巳年',
+                   '午年', '未年', '申年', '酉年', '戌年', '亥年']
+BLOOD_TYPE = ['A型', 'B型', 'O型', 'AB型']
+
+
 def main():
-    pass
+    l = []
+    for zs in ZODIAC_SIGN:
+        for jz in JAPANESE_ZODIAC:
+            for bt in BLOOD_TYPE:
+                l.append(zs + ' ✖ ' + jz + ' ✖ ' + bt)
+
+    l_random = random.sample(l, len(l))
+
+    count = 0
+    for i in range(len(l_random)):
+        count += 1
+        print('{0:<6} {1}'.format('第' + str(count) + '位:', l_random[i]))
+
 
 if __name__ == '__main__':
     main()
